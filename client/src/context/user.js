93ab62121568b710ded9e1537661b48da21d267b
alexbotha <alexbotha@hotmail.co.uk>
2023-04-26 15:07:02 +0100

    will add login logout signup functions

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -3,18 +3,26 @@ import React, { useState, useEffect } from "react";
 const UserContext = React.createContext();
 
 function UserProvider({ children }) {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch("/me")
+    fetch("http://localhost:4000/me")
       .then((res) => res.json())
       .then((data) => {
         setUser(data);
       });
   }, []);
 
+  function login() {}
+
+  function logout() {}
+
+  function signup() {}
+
   return (
-    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ user, login, logout, signup }}>
+      {children}
+    </UserContext.Provider>
   );
 }
 
