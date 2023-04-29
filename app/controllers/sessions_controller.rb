4c6c6603f0e8ef221de38c03c22f59f6e9e32809
alexbotha <alexbotha@hotmail.co.uk>
2023-04-29 15:40:39 +0100

    will add create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,19 @@ class SessionsController < ApplicationController
 
   #login
   def create 
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      render json: user
+    else 
+      render json: {error: "Invalid username or password"}, status: :unauthorized 
+    end
   end 
 
   #destroy
   def destroy 
+    session.clear
   end 
+
+
 end
