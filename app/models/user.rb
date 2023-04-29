fedc7de20bfd46f76e23c5441b9653ac0c42aba5
alexbotha <alexbotha@hotmail.co.uk>
2023-04-29 15:39:53 +0100

    adds validations to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, :email, :password, :password_confirmation, presence: :true 
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 5}
+
 
   has_many :bookings 
   has_many :hotels, through: :bookings 
