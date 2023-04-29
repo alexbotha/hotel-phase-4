9e83d260013bbcc3eebf60d253ef0b32a95c6901
alexbotha <alexbotha@hotmail.co.uk>
2023-04-29 15:40:20 +0100

    will add create and private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,14 @@ class UsersController < ApplicationController
 
   #signuppp
   def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id 
+      render json: user
+    else 
+      render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
   end 
-
+end
   #current user in json 
   def show 
     user = User.find_by(id: session[:user_id])
@@ -12,5 +18,11 @@ class UsersController < ApplicationController
     else
       render json: {error: "Not authorized"}, status: :unauthorized   
     end 
+  end 
+
+  private 
+
+  def user_params 
+    params.permit(:username, :email, :password, :password_confirmation)
   end 
 end
