a216b5434a5fb6b1f1d429151fdf67b982c1c897
alexbotha <alexbotha@hotmail.co.uk>
2023-05-03 19:23:35 +0100

    nav bar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/user";
+import { useNavigate, NavLink } from "react-router-dom";
 
 function Navbar() {
-  return <div></div>;
+  const { user, logout, loggedIn } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function logoutUser() {
+    fetch("/logout", {
+      method: "DELETE",
+      headers: { "Content-type": "application/json" },
+    }).then(() => {
+      logout();
+      navigate("/");
+    });
+  }
+
+  if (loggedIn) {
+    return (
+      <div className="navbar">
+        <h3>{`${user.username}'s account`}</h3>
+
+        <br />
+        <NavLink to="/hotels">
+          <button>Hotels</button>
+        </NavLink>
+        <NavLink to="/myaccount">
+          <button>My account </button>
+        </NavLink>
+        <button onClick={logoutUser}>Logout</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <NavLink to="/login">
+          <button>Login</button>
+        </NavLink>
+        <NavLink to="/signup">
+          <button>Signup</button>
+        </NavLink>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
