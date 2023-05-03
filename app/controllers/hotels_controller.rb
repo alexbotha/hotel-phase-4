59cd9ccc5f37a063c792f5b681bfeea66df5da7c
alexbotha <alexbotha@hotmail.co.uk>
2023-05-03 17:07:51 +0100

    will add hotel controller methods

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,29 @@
 class HotelsController < ApplicationController
+
+  def index 
+    
+    hotels = Hotel.all 
+    render json: hotels, status: :ok 
+  end 
+
+  def create 
+    hotel = Hotel.create!(hotel_params)
+    render json: hotel, status: :created
+  end 
+
+  def destroy 
+    
+    hotel = Hotel.find(params[:id])
+    booking.destroy
+    render json: {message: "Booking has been deleted."}, status: :ok
+  end 
+
+  private 
+
+  def hotel_params
+    params.permit(:name, :location, :image_url, :rating, :price, :about, :telephone, :country)
+  end 
+
 end
+
+
