b80681a83ca97e7a253f3601ac3090c314e02e3e
alexbotha <alexbotha@hotmail.co.uk>
2023-05-05 10:18:57 +0100

    sign up component workign

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,77 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/user";
 
 function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errorsList, setErrorsList] = useState([]);
+  const { signup } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+        password_confirmation: passwordConfirmation,
+      }),
+    })
+      .then((res) => res.json())
+      .then((user) => {
+        if (!user.errors) {
+          signup(user);
+          navigate("/");
+        } else {
+          const errorLis = user.errors.map((e) => <li>{e}</li>);
+          setErrorsList(errorLis);
+        }
+      });
+  }
+
   return (
     <div>
-      <h3>sign up</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          value={username}
+        />
+
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          value={email}
+        />
+
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          value={password}
+        />
+
+        <input
+          type="password"
+          name="password_confirmation"
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          placeholder="Password Confirmation"
+          value={passwordConfirmation}
+        />
+        <input type="submit" />
+      </form>
+      <ul>{errorsList}</ul>
     </div>
   );
 }
