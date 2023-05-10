60c47b2d0d7440a5437527c9dabc76b076f21086
alexbotha <alexbotha@hotmail.co.uk>
2023-05-10 09:15:31 +0100

    will add validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
- validates :check_in, :check_out, :guests, :user_id, presence: :true 
+ validates :check_in, :check_out, :guests, :user_id, :hotel_id, presence: :true 
 
   belongs_to :user
   belongs_to :hotel
