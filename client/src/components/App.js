918552caa2c4a2d42ed859ad57378a0396fc4e9c
alexbotha <alexbotha@hotmail.co.uk>
2023-05-10 09:16:56 +0100

    will add new route in routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,32 +7,34 @@ import Home from "./Home";
 import Navbar from "./Navbar";
 import Login from "./Login";
 import Signup from "./Signup";
-import BookingsContainer from "./BookingsContainer";
+import MyAccount from "./MyAccount";
 import Hotel from "./Hotel";
 import HotelsContainer from "./HotelsContainer";
+import AddBookingForm from "./AddBookingForm";
 
 function App() {
-  const { hotels } = useContext(UserContext);
+  const { hotels, loggedIn, loading } = useContext(UserContext);
   return (
     <div className="App">
       <UserProvider>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={loggedIn ? <Home /> : <Login />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/hotels" element={<HotelsContainer />} />
-          <Route exact path="/myaccount" element={<BookingsContainer />} />
+          <Route exact path="/myaccount" element={<MyAccount />} />
           <Route
+            exact
+            path="/hotels/:hotelId/bookings/new"
+            element={<AddBookingForm />}
+          />
+          {/* <Route
             exact
             path="/myaccount/bookings"
-            element={<BookingsContainer />}
-          />
-          <Route
-            exact
-            path="/hotels/:id"
-            element={hotels.length > 0 ? <Hotel /> : null}
-          />
+            element={<MyAccount />}
+          /> */}
+          <Route exact path="/hotels/:id" element={<Hotel />} />
           {/* <Route
             exact
             path=""
