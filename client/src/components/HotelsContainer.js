f91c305ad9ce3ccd1c9071cc8ba76e7233316de8
alexbotha <alexbotha@hotmail.co.uk>
2023-05-12 15:14:10 +0100

    will add usenavigate for user experience

--- client/src/components/HotelsContainer.js
+++ client/src/components/HotelsContainer.js
@@ -1,17 +1,24 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/user";
 import HotelItem from "./HotelItem";
+import { useNavigate } from "react-router-dom";
 
 function HotelsContainer() {
   const { loggedIn, hotels } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function createHotel() {
+    navigate("/hotels/new");
+  }
 
   if (loggedIn) {
     return (
       <>
         <div>
-          <h3>Hotels</h3>
+          <h3 className="">Hotels</h3>
+          <button onClick={createHotel}>Create new hotel</button>
           <br />
-          <p>There are {hotels.length} hotels to chose from</p>
+          <p className="">There are {hotels.length} hotels to chose from</p>
         </div>
         {hotels.map((hotel) => (
           <HotelItem key={hotel.id} hotel={hotel} />
