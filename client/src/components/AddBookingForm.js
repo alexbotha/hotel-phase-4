8f75656863d9231a7693a73250fabc2e65d07325
alexbotha <alexbotha@hotmail.co.uk>
2023-05-12 15:16:49 +0100

    will add conditional rendering

--- client/src/components/AddBookingForm.js
+++ client/src/components/AddBookingForm.js
@@ -2,13 +2,13 @@ import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/user";
 import { useNavigate, useParams } from "react-router-dom";
 
-function AddBookingForm({}) {
+function AddBookingForm() {
   const [checkIn, setCheckIn] = useState("");
   const [checkOut, setCheckOut] = useState("");
   const [guests, setGuests] = useState("");
-  const [overallPrice, setOverallPrice] = useState("");
+
   const [h, setH] = useState("");
-  const { addBooking, hotels } = useContext(UserContext);
+  const { addBooking, hotels, loggedIn, loading } = useContext(UserContext);
   const { hotelId } = useParams();
   const navigate = useNavigate();
 
@@ -18,7 +18,6 @@ function AddBookingForm({}) {
       check_in: checkIn,
       check_out: checkOut,
       guests: guests,
-      overallPrice: overallPrice,
       hotel_id: hotelId,
     });
     navigate("/myaccount");
@@ -29,39 +28,39 @@ function AddBookingForm({}) {
     setH(hotel);
   }, [hotelId, hotels]);
 
-  return (
-    <div className="createBooking">
-      <h3>{`Create a booking for ${h ? h.name : ""}`}</h3>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="date"
-          value={checkIn}
-          placeholder="Check in"
-          onChange={(e) => setCheckIn(e.target.value)}
-        />
-        <input
-          type="date"
-          value={checkOut}
-          placeholder="Check out"
-          onChange={(e) => setCheckOut(e.target.value)}
-        />
-        <input
-          type="number"
-          value={guests}
-          placeholder="Number of guests"
-          onChange={(e) => setGuests(e.target.value)}
-        />
-        {/* <input
-        type="integer"
-        value={overallPrice}
-        placeholder="overll"
-        onChange={(e) => setOverallPrice(e.target.value)}
-      /> */}
-        <input type="submit" />
-      </form>
-      <h2>Total price: {h.price}</h2>
-    </div>
-  );
+  if (loggedIn) {
+    return loading ? (
+      <h3>Loading...</h3>
+    ) : (
+      <>
+        <div className="createBooking">
+          <h3>{`Create a booking for ${h ? h.name : ""}`}</h3>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="date"
+              value={checkIn}
+              placeholder="Check in"
+              onChange={(e) => setCheckIn(e.target.value)}
+            />
+            <input
+              type="date"
+              value={checkOut}
+              placeholder="Check out"
+              onChange={(e) => setCheckOut(e.target.value)}
+            />
+            <input
+              type="number"
+              value={guests}
+              placeholder="Number of guests"
+              onChange={(e) => setGuests(e.target.value)}
+            />
+
+            <input type="submit" />
+          </form>
+        </div>
+      </>
+    );
+  } else return <h3>Please login</h3>;
 }
 
 export default AddBookingForm;
