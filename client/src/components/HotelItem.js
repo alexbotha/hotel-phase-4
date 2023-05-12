861b76c03abddc5b95b0f1c70a0662a99210aa62
alexbotha <alexbotha@hotmail.co.uk>
2023-05-12 15:14:28 +0100

    will add conditional rendering

--- client/src/components/HotelItem.js
+++ client/src/components/HotelItem.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/user";
 import { NavLink } from "react-router-dom";
 
 function HotelItem({ hotel }) {
-  return (
+  const { loading } = useContext(UserContext);
+  return loading ? (
+    <h3>Loading...</h3>
+  ) : (
     <div className="all-hotels">
       <img src={hotel.image_url} alt="hotel" />
       <h3>{hotel.name}</h3>
