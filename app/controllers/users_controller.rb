f4ed1970148f5cd71648387485fb187690134e8e
alexbotha <alexbotha@hotmail.co.uk>
2023-05-12 15:11:35 +0100

    will add index route for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    users = User.all
+    render json: users, include: :bookings
+  end
+
   #signuppp
   def create
     user = User.create(user_params)
