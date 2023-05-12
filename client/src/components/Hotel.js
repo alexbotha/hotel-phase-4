4a6c085f8b345dcb7428b9a4e04640184d9e840e
alexbotha <alexbotha@hotmail.co.uk>
2023-05-12 15:14:58 +0100

    building out map feature to render a hotel:id

--- client/src/components/Hotel.js
+++ client/src/components/Hotel.js
@@ -1,32 +1,46 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useContext } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import { UserContext } from "../context/user";
 
 function Hotel() {
-  const { hotels, loading } = useContext(UserContext);
-  const [hotel, setHotel] = useState([]);
+  const { hotels, loading, loggedIn } = useContext(UserContext);
   const { id } = useParams();
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    let hotel = hotels.find((h) => h.id === parseInt(id));
-    setHotel(hotel);
-  }, [id, hotels]);
+  const hotel = hotels.find((h) => h.id === parseInt(id));
 
-  return loading ? (
-    <h3 className="loading">Loading...</h3>
-  ) : (
-    <div className="one-hotel">
-      <img src={hotel.image_url} alt="hotelimage" />
-      <div className="one-hotel-text">
-        <h3>{hotel.name}</h3>
-        <p>{hotel.about}</p>
-        <p>{hotel.location}</p>
-        <p>Rating: {hotel.rating}</p>
-        <p>Price per night: {hotel.price}</p>
-        <p>Country: {hotel.country}</p>
-      </div>
-    </div>
-  );
+  function book() {
+    navigate(`/hotels/${id}/bookings/new`);
+  }
+
+  if (loggedIn) {
+    return loading ? (
+      <h3 className="loading">Loading...</h3>
+    ) : (
+      <>
+        <div className="one-hotel">
+          <img src={hotel.image_url} alt="hotelimage" />
+          <div className="one-hotel-text">
+            <h3>{hotel.name}</h3>
+            <p>{hotel.about}</p>
+            <p>{hotel.location}</p>
+            <p>Rating: {hotel.rating}</p>
+            <p>Price per night: {hotel.price}</p>
+            <p>Country: {hotel.country}</p>
+            <h3>Who's booked this?</h3>
+            {hotel.bookings.map((booking) => (
+              <li>
+                <Link to={`/users/${booking.user.id}`}>
+                  {booking.user.username}
+                </Link>
+              </li>
+            ))}
+          </div>
+        </div>
+        <button onClick={book}>Interested? Book now!</button>
+      </>
+    );
+  } else return <h3>Please login in</h3>;
 }
 
 export default Hotel;
