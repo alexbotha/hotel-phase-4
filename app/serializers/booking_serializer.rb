5396469c946c08913e526564e0190102aff5f246
alexbotha <alexbotha@hotmail.co.uk>
2023-05-17 23:28:32 +0100

    adding attrib to serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,5 +1,5 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :check_in, :check_out, :guests
+  attributes :id, :check_in, :check_out, :guests, :hotel_id, :user_id
   belongs_to :hotel 
   belongs_to :user
 end
