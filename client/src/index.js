76085839f3a778e395753669a52f109c68b39557
alexbotha <alexbotha@hotmail.co.uk>
2023-05-17 23:31:30 +0100

    moving user privder inside router

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,11 @@ import { UserProvider } from "./context/user";
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider>
-      <Router>
+    <Router>
+      <UserProvider>
         <App />
-      </Router>
-    </UserProvider>
+      </UserProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
