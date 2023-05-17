60dd07a26604e78cc54805505399bc603618fc8d
alexbotha <alexbotha@hotmail.co.uk>
2023-05-17 23:27:25 +0100

    moving fallback index to bottom

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,20 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-
-   #Custom routes
-   post '/login', to: 'sessions#create' 
-   delete '/logout', to: 'sessions#destroy'
+  
+  #Custom routes
+  post '/login', to: 'sessions#create' 
+  delete '/logout', to: 'sessions#destroy'
    post '/signup', to: 'users#create'
    get '/me', to: 'users#show' 
+   patch '/users/:id', to: 'users#update'
+    
+  #  get '/myaccount', to: 'bookings#myBookings'
  
    #Routes - full crud 
    resources :bookings 
    resources :hotels 
+   resources :users
+   
+   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
