bac1147c343fd416d647eca018bc98ae49d13e18
alexbotha <alexbotha@hotmail.co.uk>
2023-05-17 23:28:09 +0100

    will create custom method with uniq

--- app/serializers/hotel_serializer.rb
+++ app/serializers/hotel_serializer.rb
@@ -1,6 +1,9 @@
 class HotelSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :image_url, :rating, :price, :about, :telephone, :country
+  attributes :id, :name, :location, :image_url, :rating, :price, :about, :telephone, :country, :custom_users
 
+  def custom_users
+    object.users.uniq
+  end 
   
   has_many :bookings
 end
