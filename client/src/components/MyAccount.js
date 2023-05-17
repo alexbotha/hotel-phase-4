a353bd91a84c3b1f836ecdee4ad044fd151e571e
alexbotha <alexbotha@hotmail.co.uk>
2023-05-17 23:32:23 +0100

    handle delete

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -1,40 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../context/user";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import EditBooking from "./EditBooking";
 
 function MyAccount() {
-  const { loggedIn, user, loading } = useContext(UserContext);
+  const { loggedIn, user, setUser, loading } = useContext(UserContext);
+
+  function handleDelete(booking) {
+    let findBook = user.bookings.filter((b) => b.id !== booking);
+    let y = { ...user, bookings: findBook };
+
+    fetch(`/bookings/${booking}`, {
+      method: "DELETE",
+    }).then((res) => {
+      if (res.ok) {
+        setUser(y);
+      } else {
+        console.log("not yet");
+      }
+    });
+  }
 
   if (loggedIn) {
     return loading ? (
       <h3 className="loading">Loading...</h3>
     ) : (
-      <>
-        {/* <div className="my-account-details">
-          <h3>{`${user.username}'s profile`}</h3>
-          <p>{`First name: ${user.first_name}`}</p>
-          <p>{`Last name: ${user.last_name}`}</p>
-          <p>{`Email: ${user.email}`}</p>
-          <p>{`Telephone: ${user.telephone}`}</p>
-          <p>{`DOB: ${user.birth_date}`}</p>
-          <p>{`address: ${user.address}`}</p>
-          <p>{`City: ${user.city}`}</p>
-          <p>{`Post code: ${user.post_code}`}</p>
-          <p>{`Profile photo: ${user.avatar_url}`}</p>
-          <br></br>
-        </div> */}
-        <>
-          <div className="my-account-details">
-            <h3>{user.username} hotels booked</h3>
-            {user.bookings.map((booking) => (
-              <>
-                <li>{booking.hotel.name}</li>
-                <button>Edit</button>
-                <button>Delete</button>
-              </>
-            ))}
-          </div>
-        </>
-      </>
+      <div className="my-account-details">
+        <h3>{user.username} hotels booked</h3>
+        {user.bookings.map((booking) => (
+          <>
+            <div>
+              <h3>Hotel: {booking.hotel.name}</h3>
+              <li>Guests: {booking.guests}</li>
+              <li>Check in: {booking.check_in}</li>
+              <li>Check out: {booking.check_out}</li>
+              <button>
+                <Link to={`/myaccount/bookings/${booking.id}`}>Edit</Link>
+              </button>
+              <button onClick={() => handleDelete(booking.id)}>Delete</button>
+            </div>
+          </>
+        ))}
+      </div>
     );
   } else {
     return <h3>Not available</h3>;
