71c87ce62c039e970984a54d679a1e4ee955d256
alexbotha <alexbotha@hotmail.co.uk>
2023-05-24 15:15:51 +0100

    will add validations

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,12 @@
 class Hotel < ApplicationRecord
+  validates :name, :location, uniqueness: true
+  
+  validates :name, :location, :image_url, :rating, :price, :about, :telephone, :country, presence: true
+  
+  
+  # No option to delete a hotel but wanted to add this for 
+  # potential scaling post graduation
   has_many :bookings, dependent: :destroy
+  
   has_many :users, through: :bookings
 end
