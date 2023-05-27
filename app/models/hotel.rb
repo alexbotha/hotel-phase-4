7a37faf75f6eca6c1b829a570afbf10a0697c7dd
alexbotha <alexbotha@hotmail.co.uk>
2023-05-27 10:05:19 +0100

    adding number validation

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,11 +2,13 @@ class Hotel < ApplicationRecord
   validates :name, :location, uniqueness: true
   
   validates :name, :location, :image_url, :rating, :price, :about, :telephone, :country, presence: true
+
+  validates :rating, :price, :telephone, numericality: true 
   
   
-  # No option to delete a hotel but wanted to add this for 
-  # potential scaling post graduation
-  has_many :bookings, dependent: :destroy
+  # No option to delete a hotel but wanted to add this for potential scaling post graduation
+  has_many :bookings
+  #, dependent: :destroy
   
   has_many :users, through: :bookings
 end
